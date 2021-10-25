0938525e49273954fc943fed6b3ea37b0f3b5f0e
Vladislav Plotnikov <vadimovich74@gmail.com>
2021-10-25 14:11:26 +0300

    Composite Class Hierarchy has been formed

--- AssemblyBrowserProject/TreeComposite.cs
+++ AssemblyBrowserProject/TreeComposite.cs
@@ -6,16 +6,73 @@ using System.Threading.Tasks;
 
 namespace AssemblyBrowserProject
 {
+    namespace asdf
+    {
+        namespace sd
+        {
+
+        }
+    }
+
+    enum COMPONENT_TYPE{
+        FIELD,
+        PROPERTY,
+        METHOD,
+        NAMESPACE,
+        CLASS,
+        STRUCT
+    }
+
     class TreeComponent
     {
+        public TreeComponent(String name, COMPONENT_TYPE componentType)
+        {
+            Name = name;
+            ComponentType = componentType;
+        }
+
+        public virtual void Add(TreeComponent cmp) { throw new NotImplementedException(); }
+        public virtual void Remove(TreeComponent cmp) { throw new NotImplementedException(); }
+        public virtual TreeComponent GetChild(int index) { throw new NotImplementedException(); }
+
+        public String Name { get; set;  }
+        public COMPONENT_TYPE ComponentType { get; set; }
+        public virtual TreeComposite GetTreeComposite() { return null; }
     }
 
-    class TreeLeaf
+    class TreeLeaf : TreeComponent
+    {
+        public TreeLeaf(String name, COMPONENT_TYPE componentType, Type memberType) :
+            base(name, componentType)
+        {
+            MemberType = memberType;
+        }
+        public Type MemberType { get; set; }
+    }
+
+    class MethodTreeLeaf : TreeLeaf
     {
 
     }
 
-    class TreeComposite
+    class PropertyTreeLeaf : TreeLeaf
+    {
+
+    }
+
+    class FieldTreeLeaf : TreeLeaf {
+
+    }
+
+    class TreeComposite : TreeComponent
+    {
+        public override void Add(TreeComponent cmp) { throw new NotImplementedException(); }
+        public override void Remove(TreeComponent cmp) { throw new NotImplementedException(); }
+        public override TreeComponent GetChild(int index) { throw new NotImplementedException(); }
+        public List<TreeComponent> components = new List<TreeComponent>();
+    }
+
+    class NamespaceTreeComposite : TreeComposite
     {
 
     }
@@ -30,8 +87,4 @@ namespace AssemblyBrowserProject
 
     }
 
-    class NamespaceTreeComposite: TreeComposite
-    {
-
-    }
 }
--- UnitTestsProject/Program.cs
+++ UnitTestsProject/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UnitTestsProject
 {
+    namespace fas {
+        namespace asd
+        {
+            namespace zzz
+            {
+                class Foo { }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Assembly.GetExecutingAssembly().GetTypes()[1].Namespace);
+            Console.ReadLine();
         }
     }
 }

0a8eb87508ebf0d275ecd4e29f90a8cf52fea9ec
Vladislav Plotnikov <vadimovich74@gmail.com>
2021-10-25 11:25:55 +0300

    Have added Composite Classes

---
+++ AssemblyBrowserProject/AssemblyTreeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowserProject
+{
+    public class AssemblyTreeBuilder
+    {
+        public AssemblyTreeBuilder()
+        {
+
+            Assembly assembly = Assembly.GetAssembly(typeof(DateTime));
+            foreach (var exportedType in assembly.GetExportedTypes())
+            {
+                var parentNode = treeView1.Nodes.Add(exportedType.Name);
+                AddNodes(exportedType, parentNode);
+            }
+        }
+
+        private void AddNodes(Type type, TreeNode node)
+        {
+            foreach (var nestedType in type.GetNestedTypes())
+            {
+                var nestedNode = node.Nodes.Add(nestedType.Name);
+                AddNodes(nestedType, nestedNode);
+            }
+        }
+    }
+
+
+}
---
+++ AssemblyBrowserProject/TreeComposite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowserProject
+{
+    class TreeComponent
+    {
+    }
+
+    class TreeLeaf
+    {
+
+    }
+
+    class TreeComposite
+    {
+
+    }
+
+    class StructTreeComposite : TreeComposite
+    {
+
+    }
+
+    class ClassTreeComposite : TreeComposite
+    {
+
+    }
+
+    class NamespaceTreeComposite: TreeComposite
+    {
+
+    }
+}

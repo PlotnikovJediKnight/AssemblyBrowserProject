2bd9ff37bd2ca7b9ccdbc48007663d51fce5bd1b
Vladislav Plotnikov <vadimovich74@gmail.com>
2021-10-27 00:20:00 +0300

    Have implemented WPF

--- AssemblyBrowserWPF/MainWindow.xaml.cs
+++ AssemblyBrowserWPF/MainWindow.xaml.cs
@@ -1,28 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AssemblyBrowserProject;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AssemblyBrowserWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+			ApplicationViewModel avm = new ApplicationViewModel();
+			avm.hWnd = this;
+			DataContext = avm;
         }
-    }
+
+		public void GetTreeFilled(TreeComponent tree)
+        {
+			trvStructure.Items.Add(CreateTreeItem(tree.GetTreeComposite()));
+        }
+
+		public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+		{
+			TreeViewItem item = e.Source as TreeViewItem;
+			if ((item.Items.Count == 1) && (item.Items[0] is string))
+			{
+				item.Items.Clear();
+
+				TreeComposite expandedNode = null;
+				if (item.Tag is TreeComposite)
+				{
+					expandedNode = (item.Tag as TreeComposite).GetTreeComposite();
+
+					foreach (TreeComponent subDir in expandedNode.components)
+						item.Items.Add(CreateTreeItem(subDir));
+				}
+			}
+		}
+
+		private TreeViewItem CreateTreeItem(object o)
+		{
+			TreeViewItem item = new TreeViewItem();
+			item.Header = o.ToString();
+			item.Tag = o;
+			if (o is TreeComposite) item.Items.Add("Loading...");
+			return item;
+		}
+	}
 }
